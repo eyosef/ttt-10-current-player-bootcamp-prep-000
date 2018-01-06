18f8f5156a92be6d041cd8c009af9e3d98fe4b56
Eden Mesfun <emesfun@gmail.com>
2018-01-06 02:16:56 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+require 'pry'      
+
+def turn_count(board)
+  counter = 0 
+  board.each do |index|
+    counter <= 8
+    # binding.pry
+  if index == " "
+    counter += 0
+  elsif index == "X"
+    counter += 1 
+  else
+    counter += 1 
+    end 
+  end
+  counter 
+end 
