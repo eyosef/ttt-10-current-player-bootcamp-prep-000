5036c6673d346787b8d7583bcc9062774dfbf3a0
Eden Mesfun <emesfun@gmail.com>
2018-01-06 02:24:26 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -20,7 +20,7 @@ def current_player(board)
   if turn_count(board) == "O"
     return current_player("X")
   elsif turn_count(board) == "X"
-    return current_player("O")
+    return current_player("X")
   else 
     return nil 
   end 
